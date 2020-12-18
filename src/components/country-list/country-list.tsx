8ec90859a68078191a511ff7f55b102cf13594d5
SonKn1ght <maximiilian321@gmail.com>
2020-12-18 19:02:37 +0300

    fix: for update from develop

--- src/components/country-list/country-list.tsx
+++ src/components/country-list/country-list.tsx
@@ -1,19 +1,24 @@
 import * as React from 'react';
 import './country-list.css';
 import { useSelector } from 'react-redux';
+import SimpleBar from 'simplebar-react';
+import 'simplebar/dist/simplebar.min.css';
 import { IAppState } from '@types';
 
 export const CountryList = () => {
   const country = useSelector((state: IAppState) => state.countries);
 
   return <ul className='country-list'>
-    {
-      country.map(({name, flag, population}) => (
-      <li key={name}>
-        <img className='country-list__image' src={flag} alt={name} />
-        {name}
-      </li>
-      ))
-    }
+    <SimpleBar forceVisible="false" className='country-list__size-scrollbar'>
+      {
+        country.map(({name, flag, population}) => (
+          <li key={name}>
+            <img className='country-list__image' src={flag} alt={name} />
+            {name}
+
+          </li>
+        ))
+      }
+    </SimpleBar>
   </ul>
 }
