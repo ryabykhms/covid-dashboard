d9c8e99058a44dd7a6398c9089f44f8587eb3dfb
ryabykhms <ryabykhms@gmail.com>
2020-12-21 21:47:14 +0300

    feat: change reducers and app props

--- src/store/reducers/countries-reducer.ts
+++ src/store/reducers/countries-reducer.ts
@@ -1,7 +1,10 @@
 import { Dispatch } from 'redux';
 import { defaultState } from '@store';
-import { AppActions, ICountryCovidItem } from '@types';
-import { storage, func } from '@utils';
+import { AppActions, IFetchResult } from '@types';
+import { storage } from '@utils';
+import { api } from '@api';
+
+let populationMain: any = 0;
 
 export function countriesReducer(state = defaultState, action: any) {
   const { payload, type } = action;
@@ -23,11 +26,15 @@ export function countriesReducer(state = defaultState, action: any) {
 
     case AppActions.SET_ACTIVE_COUNTRY:
       let covidActive = state.covidActive;
+      let selected = state.selectedData;
 
       if (payload) {
+        populationMain =
+          state.covidAllCountries[payload.toUpperCase()].population;
         covidActive = state.covidAllCountries[payload.toUpperCase()];
       } else {
         covidActive = state.covidGlobal;
+        selected = state.globalCovidData;
       }
 
       return {
@@ -35,21 +42,21 @@ export function countriesReducer(state = defaultState, action: any) {
         isCountryCovidDataLoaded: false,
         selectedCountry: payload,
         covidActive,
+        selectedData: selected,
       };
 
     case AppActions.SET_COUNTRY_COVID_DATA:
       let selectedData = state.selectedData;
-      let population: any = 0;
       let selectedCountry: any = state.selectedCountry;
 
-      if (selectedCountry) {
-        population = state.covidAllCountries[selectedCountry].population;
-      }
-
       if (!payload.isError) {
-        selectedData = formatCovidData(population, payload.data);
+        selectedData = payload.data;
       } else {
         selectedData = null;
+      }
+
+      if (selectedCountry) {
+        populationMain = state.covidAllCountries[selectedCountry].population;
       }
 
       return {
@@ -63,92 +70,30 @@ export function countriesReducer(state = defaultState, action: any) {
   }
 }
 
-function addNewAndPer100ThousandData(
-  population: number,
-  props: ICountryCovidItem,
-  prevDayProps: ICountryCovidItem
-) {
-  if (!prevDayProps) {
-    return {};
-  }
-
-  const NewConfirmed = props.Confirmed - prevDayProps.Confirmed;
-  const NewDeaths = props.Deaths - prevDayProps.Deaths;
-  const NewRecovered = props.Recovered - prevDayProps.Recovered;
-
-  const result = {
-    ConfirmedPer100: func.calcPer100Thousand(population, props.Confirmed),
-    DeathsPer100: func.calcPer100Thousand(population, props.Deaths),
-    RecoveredPer100: func.calcPer100Thousand(population, props.Recovered),
-    NewConfirmed,
-    NewDeaths,
-    NewRecovered,
-    NewConfirmedPer100: func.calcPer100Thousand(population, NewConfirmed),
-    NewDeathPer100: func.calcPer100Thousand(population, NewDeaths),
-    NewRecoveredPer100: func.calcPer100Thousand(population, NewRecovered),
-  };
-
-  return result;
-}
-
-function formatCovidData(population: number, data: ICountryCovidItem[]) {
-  return data.map(({ Date: date, ...props }, i) => ({
-    ...props,
-    ...addNewAndPer100ThousandData(population, data[i], data[i - 1]),
-    Date: new Date(date),
-  }));
-}
-
 export const setActiveCountry = (payload: string | null) => (
   dispatch: Dispatch
 ) => {
   dispatch({ type: AppActions.SET_ACTIVE_COUNTRY, payload });
-  return fetch(`https://api.covid19api.com/country/${payload}`)
-    .then(async (response) => {
-      dispatch({
-        type: AppActions.SET_COUNTRY_COVID_DATA,
-        payload: {
-          isError: false,
-          data: await response.json(),
-        },
-      });
-    })
-    .catch((error) => {
-      dispatch({
-        type: AppActions.SET_COUNTRY_COVID_DATA,
-        payload: {
-          isError: true,
-          data: error,
-        },
-      });
-    });
+
+  return api.fetchCovidCountryData(
+    AppActions.SET_COUNTRY_COVID_DATA,
+    (resultObject: IFetchResult) => {
+      dispatch(resultObject);
+    },
+    (errorObject: IFetchResult) => dispatch(errorObject),
+    { country: payload, population: populationMain }
+  );
 };
 
 export const loadCountries = () => (dispatch: Dispatch) => {
   if (!storage.countries.has()) {
-    fetch(
-      'https://restcountries.eu/rest/v2/all?fields=name;population;flag;alpha2Code'
-    )
-      .then(async (response) => {
-        dispatch({
-          type: AppActions.SET_COUNTRIES,
-          payload: {
-            isFetch: true,
-            isError: false,
-            data: await response.json(),
-          },
-        });
-      })
-      .catch((error) => {
-        dispatch({
-          type: AppActions.SET_COUNTRIES,
-          payload: {
-            isFetch: true,
-            isError: true,
-            data: error,
-          },
-        });
-      });
+    api.fetchCountries(
+      AppActions.SET_COUNTRIES,
+      (resultObject: IFetchResult) => {
+        dispatch(resultObject);
+      },
+      (errorObject: IFetchResult) => dispatch(errorObject)
+    );
   } else {
     dispatch({
       type: AppActions.SET_COUNTRIES,
--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,9 +1,14 @@
 import { defaultState } from '@store';
+import { api } from '@api';
 import { countriesReducer } from './countries-reducer';
 import { covidReducer } from './covid-reducer';
 import { selectedOptionsReducer } from './selected-options-reducer';
 
 export function rootReducer(state = defaultState, action: any) {
+  const { HOPKINS, RESTCOUNTRIES } = api.endpoints;
+  api.setCovidDataSource(HOPKINS);
+  api.setCountriesDataSource(RESTCOUNTRIES);
+
   let currentState: any = countriesReducer(state, action);
   currentState = covidReducer(currentState, action);
   currentState = selectedOptionsReducer(currentState, action);
