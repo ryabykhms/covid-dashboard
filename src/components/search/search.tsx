84ab8031bf2885004043904aa9825ef845695851
SonKn1ght <maximiilian321@gmail.com>
2020-12-23 15:03:18 +0300

    fix: layout

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -12,7 +12,7 @@ export const Search = () => {
   }
 
   return <div className='search'>
-    <h3>Search</h3>
+    <h3>Country Search</h3>
     <input type="text" onInput={setSearch} />
   </div>
 }
