73574ecdca21dc4529e9e5814f5835636bd33da3
ryabykhms <ryabykhms@gmail.com>
2020-12-26 00:26:11 +0300

    feat: add virtual keyboard

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,18 +1,42 @@
 import * as React from 'react';
+import { FormEvent, useEffect, useRef } from 'react';
+import { useDispatch } from 'react-redux';
+import { setSearchValue } from '@store';
+import { Keyboard } from '../../virtual-keyboard';
 import './search.css';
-import { FormEvent } from "react";
-import { useDispatch } from "react-redux";
-import { setSearchValue } from "@store";
 
 export const Search = () => {
   const dispatch = useDispatch();
   const setSearch = (evt: FormEvent) => {
     const target = evt.target as HTMLFormElement;
     dispatch(setSearchValue(target.value));
-  }
+  };
 
-  return <div className='search'>
-    <h3>Country Search</h3>
-    <input type="text" onInput={setSearch} />
-  </div>
-}
+  useEffect(() => {
+    Keyboard.init();
+  }, []);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleKeyboardOpen = () => {
+    if (!inputRef.current) {
+      return;
+    }
+
+    try {
+      Keyboard.open(inputRef.current.value, undefined, undefined, inputRef.current);
+    } catch (error) {}
+  };
+
+  return (
+    <div className="search">
+      <h3>Country Search</h3>
+      <div className="search__controls">
+        <input type="text" onInput={setSearch} ref={inputRef} />
+        <button className="search__keyboard-button" onClick={() => handleKeyboardOpen()}>
+          <i className="material-icons">keyboard</i>
+        </button>
+      </div>
+    </div>
+  );
+};
---
+++ src/virtual-keyboard/sounds/ru/index.js
@@ -0,0 +1,19 @@
+import def from './default.wav';
+import caps from './caps.wav';
+import backspace from './backspace.wav';
+import enter from './enter.wav';
+import lang from './lang.wav';
+import shift from './shift.wav';
+import sounds from './sounds.wav';
+import space from './space.wav';
+
+export const ru = {
+  def,
+  caps,
+  backspace,
+  enter,
+  lang,
+  shift,
+  sounds,
+  space,
+};
